3555ae32f7374b4a6b9dde94710e3677e0f9b804
Subodh Kumar <subodhranjan98@gmail.com>
2026-03-23 16:43:00 +0530

    Design sign up pages and connect it to homepage #7

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FMS-Home</title>
+</head>
+<body>
+    <h1>Flower Management System - Home</h1>
+    <p><a href="/src/app/pages/signup.php">Sign up</a></p>
+</body>
+</html>
---
+++ src/app/pages/signup.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="form">
+            <form action="">
+                <h2>Create a new account</h2>
+                <div class="input-group">
+                    <input type="email" id="Email id" name="Email id" placeholder="Email id " required>
+                </div>
+
+                <div class="input-group">
+                    <input type="password" id="password" name="password" placeholder="password">
+
+                </div>
+                <div class="btn">
+                    <button type="submit">log in</button>
+
+                </div>
+
+                <div class="remember">
+                    <lable> <input type="checkbox">Rememeber me</lable>
+                    <a href="#">forget password?</a>
+                    <p>Don't have a account?<a href="#">logn in</a></p>
+                </div>
+
+            </form>
+        </div>
+
+    </div>
+</body>
+
+</html>
