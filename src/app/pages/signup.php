46893cb389aeadf7a8195858fe6949013f53c0f7
Subodh Kumar <subodhranjan98@gmail.com>
2026-03-24 15:11:52 +0530

    Design sign up pages and connect it to homepage Fixes #7

--- src/app/pages/signup.php
+++ src/app/pages/signup.php
@@ -1,41 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-
+    <title>FSMS-Sign-up</title>
+<link rel="stylesheet" href="/assets/css/style.css">
 <body>
-    <div class="wrapper">
-        <div class="form">
-            <form action="">
-                <h2>Create a new account</h2>
-                <div class="input-group">
-                    <input type="email" id="Email id" name="Email id" placeholder="Email id " required>
-                </div>
+    <div class="form-container">
+        <form action=""class="form">
+                    <h2>Create new acount</h2>
 
-                <div class="input-group">
-                    <input type="password" id="password" name="password" placeholder="password">
+            <input type="email"id="email-id"name="email-id"placeholder="Email-id"required>
+            <input type="password"id="pass"name="pass"placeholder="password"reqired>
+            
+            <button type="submit">login<a href="" log-in</button>
 
-                </div>
-                <div class="btn">
-                    <button type="submit">log in</button>
-
-                </div>
-
-                <div class="remember">
-                    <lable> <input type="checkbox">Rememeber me</lable>
-                    <a href="#">forget password?</a>
-                    <p>Don't have a account?<a href="#">logn in</a></p>
-                </div>
-
-            </form>
-        </div>
+        </form>
 
     </div>
+    
 </body>
-
 </html>
